fb69a72293b7d8c615b8ad58440ff82954e370c0
daniel-w0 <vulcan-dev@proton.me>
2026-06-19 06:52:51 +0100

    get valid mp4 from screen recording on windows 7

--- src/platform/win32/win32_screencap.cc
+++ src/platform/win32/win32_screencap.cc
@@ -352,7 +352,11 @@ void sc_start_recording(const sc_rect& rect) {
     std::string savePathStr = savePath.string();
 
     char cmd[1024];
-    snprintf(cmd, sizeof(cmd), "\"%s\" -y -f gdigrab -framerate 30 -offset_x %d -offset_y %d -video_size %dx%d -i desktop -c:v libx264 -pix_fmt yuv420p \"%s\"", ffmpegPath, rect.x, rect.y, w, h, savePathStr.c_str());
+    if (_sc_is_win10_or_greater()) {
+        snprintf(cmd, sizeof(cmd), "\"%s\" -y -f gdigrab -framerate 30 -offset_x %d -offset_y %d -video_size %dx%d -i desktop -c:v libx264 -pix_fmt yuv420p \"%s\"", ffmpegPath, rect.x, rect.y, w, h, savePathStr.c_str());
+    } else {
+        snprintf(cmd, sizeof(cmd), "\"%s\" -y -f gdigrab -framerate 30 -offset_x %d -offset_y %d -video_size %dx%d -i desktop -c:v mpeg4 -qscale:v 4 \"%s\"", ffmpegPath, rect.x, rect.y, w, h, savePathStr.c_str());
+    }
 
     HANDLE hCon = CreateFileA("CONOUT$", GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, &sa, OPEN_EXISTING, 0, nullptr);
     bool haveConsole = (hCon != INVALID_HANDLE_VALUE);
